a4e60a2ed25242cafd202c84e244f555d871e932
proyectowebuned <pgarcia118@alumno.uned.es>
2023-01-04 13:19:37 +0100

    setupmetas

---
+++ src/main/java/com/metas/marketing/MetasMarketingApplication.java
@@ -0,0 +1,19 @@
+//CREAREMOS CALENDARIO CON CALENDARFX (LANDER ES OTRA OPCION PERO NO VI MUCHA COMUNIDAD) LIBRERIA ABIERTA PARA 
+//CALENDARIOS Y AGENDAS QUE USA JAVAFX (MEJOR QUE SWING QUE ES MAS ANTIGUA Y LA USA JCALENDAR)
+//CREO QUE SE PODRÍA VINCULAR CALENCARIO CON API DEL DE GOOGLE, NO SE
+//AQUI HACE CALENDARIOS ANGULAR https://programacion.net/articulo/calendario_de_eventos_con_angularjs_1216
+
+
+package com.metas.marketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetasMarketingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetasMarketingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/metas/marketing/MetasMarketingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metas.marketing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetasMarketingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
